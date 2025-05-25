774f465ff93fa70029f90d1822e1873f387311ed
Gbedemah Richmond <419richmon@gmail.com>
2025-05-25 20:40:46 +0000

    more calculations

---
+++ 9-add.js
@@ -0,0 +1,9 @@
+function add(a, b) {
+
+    return a + b;
+}
+
+const firstInteger = parseInt(process.argv[2]);
+
+const secondInteger = parseInt(process.argv[3]);
+console.log(add(firstInteger, secondInteger));
