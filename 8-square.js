8de28b52db07c423ed4e34984dfcda59f923c675
Gbedemah Richmond <419richmon@gmail.com>
2025-05-25 20:38:40 +0000

    This does some calcs

--- 8-square.js
+++ 8-square.js
@@ -1,14 +1,18 @@
 const argument = process.argv[2];
 
+
 const size = parseInt(argument);
-.
+
 if (isNaN(size)) {
     console.log("Missing size");
 }
+
 else if (size > 0) {
-    
+
     for (let i = 0; i < size; i++) {
         let row = ""; 
+
+
         for (let j = 0; j < size; j++) {
             row += "X";
         }
