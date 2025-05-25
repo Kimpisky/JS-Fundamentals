856f9db8650f978d785a6f7f9e22ec521a0db503
Gbedemah Richmond <419richmon@gmail.com>
2025-05-25 20:25:42 +0000

    This works fine

---
+++ 7-multi_c.js
@@ -0,0 +1,19 @@
+
+const argument = process.argv[2];
+
+const numberOfOccurrences = parseInt(argument);
+
+if (isNaN(numberOfOccurrences)) {
+    console.log("Missing number of occurrences");
+} else {
+    let outputString = "";
+
+    // Loop 'numberOfOccurrences' times
+    for (let i = 0; i < numberOfOccurrences; i++) {
+        outputString += "C is fun";
+        if (i < numberOfOccurrences - 1) {
+            outputString += "\n";
+        }
+    }
+    console.log(outputString);
+}
