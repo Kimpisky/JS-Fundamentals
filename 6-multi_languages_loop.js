ec4f7312db251576a63f1226bbd9f4e8875e3c09
Gbedemah Richmond <419richmon@gmail.com>
2025-05-25 20:22:43 +0000

    This loops through a list

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -3,6 +3,14 @@ const messages = [
     "Python is cool",
     "JavaScript is amazing"
 ];
-const outputString = messages.join('\n');
 
-console.log(outputString);
+let combinedOutput = "";
+
+// Loop through the array
+for (let i = 0; i < messages.length; i++) {
+    combinedOutput += messages[i] + "\n";
+}
+
+const finalString = combinedOutput.slice(0, -1);
+
+console.log(finalString);
